a4c49c25ceb4aac40282e30efa5cef9aca3b4a00
Unknown <jme919@gmail.com>
2018-03-04 12:11:10 -0500

    change else statement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 var $corrArr = ["philippines", "murray", "brazil", "usa", "jordan", "montevideo", "tunis", "mount kilimanjaro", "north america", "senegal"];
-var $numCorr = 0;
-var $numWrong = 0;
-var $numUnanswered = 0;
+var $numCorr = 0;               //number of correct answers//
+var $numWrong = 0;				//number of incorrect answers//							
+var $numUnanswered = 0;			//number of questions that were not answered at all//
+var $picked;					//the radio button that was chosen//
+var $count = 0;                 //keep track of questions answered//
 
 
 
@@ -10,6 +12,38 @@ var $numUnanswered = 0;
 
 $(function() {
 	console.log("Its alive");
+
+	$(":radio").on("click",function(){
+
+		var $picked = $(this).val();
+
+		console.log("Picked: " + $picked);
+		$count++;
+		console.log($count);
+
+
+	for (var i = 0; i < $corrArr.length; i++) {
+		if($picked == $corrArr[i]){
+			console.log("correct!");
+			$numCorr++;
+			console.log($numCorr);
+
+		}
+
+	} else {
+		console.log("wrong!");
+		$numWrong++;
+		console.log("")
+	}
+    
+
+
+
+
+	});
+	
+
+	
 
 
 
