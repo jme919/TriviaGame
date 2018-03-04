2f2db49b19f9cb90667fe69b7cd3452459a84a3b
Unknown <jme919@gmail.com>
2018-03-04 10:11:04 -0500

    defined inital variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
-console.log("Its alive");
+var $corrArr = ["philippines", "murray", "brazil", "usa", "jordan", "montevideo", "tunis", "mount kilimanjaro", "north america", "senegal"];
+var $numCorr = 0;
+var $numWrong = 0;
+var $numUnanswered = 0;
+
+
+
+
+
+
+$(function() {
+	console.log("Its alive");
+
+
+
+
+});
