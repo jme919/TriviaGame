3ec2cf8938a72b664af9ecbfda8908288392a089
Unknown <jme919@gmail.com>
2018-03-05 18:50:19 -0500

    create new div for score, change jquery-move for loop to finish.onclick, create timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ var $timer = $("#timer");
 var $start = $("#start");
 var $main = $("#main");
 var answerArr = [];   
-
+var $finish = $("#finish");
 
 
 //create a timer//
@@ -17,6 +17,51 @@ var answerArr = [];
 $(function() {
 	console.log("Its alive");
 
+var count=80;
+
+var counter;
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  { counter =setInterval(timer, 1000); //1000 will  run it every 1 second
+     clearInterval(counter);
+     $main.css("display","none");
+		console.log($finish);
+		$score.css("display","inline");
+		console.log($main);
+		$timer.css("display","none");
+
+	
+
+
+
+
+		//compare the two arrays to see if answer is correct//
+	for (var i = 0; i < $corrArr.length; i++) {
+		if(answerArr[i] === $corrArr[i]){
+			console.log("correct");
+			$numCorr++;
+			console.log($numCorr);
+			$("#right").html($numCorr);
+
+		} else  {
+		console.log("wrong!");
+		$numWrong++;
+		console.log($numWrong);
+		$("#wrong").html($numWrong);
+	    }
+
+	} 
+
+     return;
+  }
+
+  // showing the number of seconds here
+   document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+}
+
 
 	$start.on("click", function(){
 		$start.css("display","none");
@@ -24,11 +69,12 @@ $(function() {
 		$main.css("display","inline");
 		console.log($main);
 		$timer.css("display","inline");
+		timer();
 
 	});
 
 
-	//create a function for a timer//
+	
 
 
 	
@@ -42,36 +88,39 @@ $(function() {
 
 		answerArr[$(this).attr("name")] = $(this).val();
 		console.log(answerArr);
-		
-		
+
+	
+    
+	});
+
+	$finish.on("click", function(){
+		$main.css("display","none");
+		console.log($finish);
+		$score.css("display","inline");
+		console.log($main);
+		$timer.css("display","none");
+
+	
+
+
+
 
 		//compare the two arrays to see if answer is correct//
 	for (var i = 0; i < $corrArr.length; i++) {
-		if(answerArr === $corrArr[i]){
+		if(answerArr[i] === $corrArr[i]){
 			console.log("correct");
 			$numCorr++;
 			console.log($numCorr);
+			$("#right").html($numCorr);
 
-		//} else  {
-		//console.log("wrong!");
-		//$numWrong++;
-		//console.log($numWrong);//
-	}
+		} else  {
+		console.log("wrong!");
+		$numWrong++;
+		console.log($numWrong);
+		$("#wrong").html($numWrong);
+	    }
 
 	} 
-
-	for (var i = 0; i < answerArr.length; i++) {
-		if(answerArr[i] != $corrArr[i]){
-			console.log("wrong");
-			$numWrong++;
-			console.log($numWrong);
-		}
-	}
-    
-
-
-
-
 	});
 	
 
